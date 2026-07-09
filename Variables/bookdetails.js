ed54e92b6ddec35951a35783640f241af74f565e
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-09 11:55:59 +0530

    Add files via upload

---
+++ Variables/bookdetails.js
@@ -0,0 +1,7 @@
+let Bookname = "Concept of physics";
+let author ="H.C. varma";
+let price = 100;
+let isavailable = true;
+console.log("We are selling a book whose name is " + Bookname + ". It is a famous book whose author name is " + author + ". its price is $" +price+ ". You can buy it anytime");
+
+
