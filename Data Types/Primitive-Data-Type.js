4c300f62625c132b2187a5e63f799638af042f77
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-12 16:47:37 +0530

    Add files via upload

---
+++ Data Types/Primitive-Data-Type.js
@@ -0,0 +1,17 @@
+// Primitive data types
+
+let name = "sanjana maurya"
+let age = 20;
+let isStudent = true;
+let money;
+let data = null ;
+let amount = 1234567890n;
+let id = Symbol("id");
+
+console.log(typeof name); //string
+console.log(typeof age); //number
+console.log(typeof isStudent); //boolean
+console.log(typeof money); //undefined
+console.log(typeof data); //object
+console.log(typeof amount); //bigint
+console.log(typeof id); //symbol
