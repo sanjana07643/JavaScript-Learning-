0678c4651ad9a8928ce0d14a1242fc2e526e5d49
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-12 15:52:45 +0530

    Add files via upload

---
+++ Operators/Comparision-operator.js
@@ -0,0 +1,13 @@
+//comparision oprrator
+
+let a = 10;
+let b = 5;
+console.log(a == b); // false
+console.log(a != b); // true
+
+let c = 10;
+let d = 6;
+console.log(c === d); //false
+console.log(c !== d); //true
+
+
