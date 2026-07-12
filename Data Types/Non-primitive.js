4c0f5c6c7080ebe8ad0700dabc07b0db9a535232
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-12 16:48:03 +0530

    Add files via upload

---
+++ Data Types/Non-primitive.js
@@ -0,0 +1,12 @@
+let student ={
+    name : " sanjana maurya",
+    age : 20,
+    cgpa : 7.2,
+    isPass : true,
+};
+console.log(student.name);
+console.log(student.age);
+console.log(student.cgpa);
+console.log(student.isPass);
+console.log(student); //all value print
+
