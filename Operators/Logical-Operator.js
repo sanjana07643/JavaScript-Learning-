9559679c3d8d16cb4c009cf57d7ee1529250856d
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-12 15:53:22 +0530

    Add files via upload

---
+++ Operators/Logical-Operator.js
@@ -0,0 +1,15 @@
+//Logical operator
+
+let a = 6;
+let b = 7;
+let condition1 = a<b;
+let condition2 = a === b;
+let condition3 = a>b;
+let condition4 = a == b;
+console.log("Logical AND = "condition1 && condituon2); 
+console.log("Logical OR = " condition1 || condition3);
+console.log("")
+
+
+
+
