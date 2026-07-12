3fd048d47bf46e42e009108ffad63aec06c9e30a
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-12 15:51:24 +0530

    Add files via upload

---
+++ Operators/arithmetic-operator.js
@@ -0,0 +1,16 @@
+//Arithmetic operator
+
+let a = 5;
+let b = 2;
+let sum = a +b;
+let subtraction = a-b;
+let multiplications = a*b;
+let division = a/b;
+let modulus = a%b;
+console.log(sum);
+console.log(subtraction);
+console.log(multiplications);
+console.log(division);
+console.log(modulus);
+
+
