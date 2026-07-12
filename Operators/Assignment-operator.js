70d262ad260c9b7f8b20ab3fbb41e926f42d4345
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-12 15:51:59 +0530

    Add files via upload

---
+++ Operators/Assignment-operator.js
@@ -0,0 +1,11 @@
+// Assignment operators
+
+let a = 5;
+let b = 5;
+console.log(a = b); //5
+console.log(a += 5); //10
+console.log(a -= 2); //8
+console.log(a *= 5); //5
+console.log(a %= 2); //0
+console.log(a**b); //0
+
