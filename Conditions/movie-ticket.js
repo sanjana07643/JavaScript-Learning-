bef1cb6c4642f4fda1fae3dbd7616f3d30e307c7
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-13 15:42:25 +0530

    Add files via upload

---
+++ Conditions/movie-ticket.js
@@ -0,0 +1,12 @@
+// movie ticket price calculator
+
+let age = prompt("Enter your age: ");
+
+if( age>0 && age<=12){
+    console.log("ticket price : 100₹");
+}else if(age>12 && age<=25){
+    console.log("ticket price : 200₹");
+}else{
+    console.log("ticket price : 300₹");
+}
+
