3841da0665d3f4e20494d770d91f6f343bc1d5ae
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-13 15:43:55 +0530

    Add files via upload

---
+++ Conditions/shopping-discount.js
@@ -0,0 +1,12 @@
+//check discount in shopping price
+
+let price = 1000;
+
+if(price>=1000 && price<5000){
+    console.log("10% discount");
+}else if ( price>=5000 && price<10000){
+    console.log("20% discount");
+}else{
+    console.log("30% discount")
+}
+
