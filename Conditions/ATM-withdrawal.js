fae930629007644ed7642324cf5e915b8d26965b
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-13 15:42:02 +0530

    Add files via upload

---
+++ Conditions/ATM-withdrawal.js
@@ -0,0 +1,12 @@
+// check balance and remaining balance.
+
+let balance = 10000;
+let withdrawal = 960;
+let remaining_balance = balance - withdrawal;
+
+if(balance>0){
+    console.log("Balance available: " +remaining_balance);
+}else {
+    console.log("balance not available");
+}
+
