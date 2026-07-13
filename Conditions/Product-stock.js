1e2032d9ed233bedd4e5943f0d9b3d748d33cda5
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-13 15:43:30 +0530

    Add files via upload

---
+++ Conditions/Product-stock.js
@@ -0,0 +1,14 @@
+// check product stock available or not
+
+let stock = 15;
+
+if( stock=0){
+    console.log("Out of stock");
+}
+if(stock <15){
+    console.log("Only few left");
+}
+else{
+    console.log(Unavailable);
+}
+
