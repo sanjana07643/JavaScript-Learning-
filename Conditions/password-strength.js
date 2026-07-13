65f2ed47b3af6ea7ee58da0d44c814f47a564284
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-13 15:42:51 +0530

    Add files via upload

---
+++ Conditions/password-strength.js
@@ -0,0 +1,19 @@
+//chack password strength and password is weak or strong
+
+let password = "abc98655676";
+console.log(password.length);
+
+if(password.length >8){
+    console.log("strong password");
+}else{
+    console.log("weak password");
+}
+
+let Password = "acb356";
+console.log(Password.length);
+
+if(Password.length >8){
+    console.log("strong password");
+}else{
+    console.log("weak password");
+}
