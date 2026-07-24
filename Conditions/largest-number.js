a3ae3626c4ed346142a9a4eafd7b7698559a94c2
Sanjana Maurya <sanjanamaurya200607@gmail.com>
2026-07-24 23:54:27 +0530

    Add files via upload

---
+++ Conditions/largest-number.js
@@ -0,0 +1,13 @@
+// Print the largest number
+
+let a = 4;
+let b = 7;
+let c = 8;
+if (a > b && a >c){
+    console.log(" a is large");
+} else if( b> a && b >c){
+    console.log(" b is large");
+}else{
+    console.log("c is large");
+}
+
